5b89f9f79b05d7c22157408d1800d60bd13ccfad
Carlos Echeverría <carlos@Dolores.local>
2020-04-29 12:05:24 +0200

    basic skeleton for returning strongly connected components using stellar obs

--- main.py
+++ main.py
@@ -7,18 +7,10 @@ from aiohttp import web
 async def handle(request):
 
     nodes = so.nodes.get_nodes_from_stellarbeat()
-    print(nodes)
     nodes_by_public_key = so.nodes.get_nodes_by_public_key(nodes)
-    print(nodes_by_public_key)
-    # dependencies_trans = nodes.get_node_dependencies(nodes_by_public_key, 'E')
-    # print(dependencies_trans)
-    # dependencies_intrans = nodes.get_node_dependencies(nodes_by_public_key, 'E', dependencies=set(), transitive=False)
-    # print(dependencies_intrans)
     # graph = so.nodes.get_trust_graph(nodes_by_public_key)
-    # print(graph)
     # strcc = scc.get_strongly_connected_components(graph)
-    # print(strcc)
-    response_obj = { 'status' : 'successful', 'graph' : nodes }
+    response_obj = { 'status' : 'successful', 'nodes_by_pubkey' : nodes_by_public_key }
 
     return web.Response(text=json.dumps(response_obj))
 
